3dc5a2389b4baf0926d441e537be4dc5edb54121
sakakibararogerio <130939147+sakakibararogerio@users.noreply.github.com>
2023-12-31 09:00:11 -0300

    Atualizando
    Atualizando

---
+++ 11_HOOKS/hooks/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HookUseStates from '../components/HookUseStates'
+
+const Home = () => {
+  return (
+    <div>
+        <HookUseStates />
+    </div>
+  )
+}
+
+export default Home
